5e98b76f2fda62a88e1bdd22c49b32f96a474bfc
MacMlyn <mlynarskimac@gmail.com>
2026-05-20 22:06:34 +0200

    fix: add pointerEvents none to HeartIcon SVG so button click registers

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -235,6 +235,7 @@ function HeartIcon({ filled, matched }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      style={{ pointerEvents: 'none' }}
     >
       <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
     </svg>
